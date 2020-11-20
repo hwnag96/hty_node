1be4e3721b725d0c1c1fedb6bf29696862cc7e4e
hwnag96 <hty0186@gmail.com>
2020-11-20 14:47:45 +0900

    db delete

--- db.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var db = mysql.createConnection({
-    host: 'ls-4ab94082220316df9f6805e2cb989baaea4e9715.cu0xyssgzj43.ap-northeast-2.rds.amazonaws.com',
-    user: 'dbmasteruser',
-    password: 'angksehwjsqq',
-    database: 'BU'
-});
-db.connect()
-module.exports = db;
